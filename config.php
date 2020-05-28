3c01dee8365622df68a5512632b474553742836f
Askariot <magicjoke@gmail.com>
2020-05-28 10:49:34 +0300

    28.05 #0

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://prostavki-shop.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://prostavki-shop.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vikki/data/www/prostavki-shop.com.ua/catalog/');
+define('DIR_SYSTEM', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/');
+define('DIR_IMAGE', '/var/www/vikki/data/www/prostavki-shop.com.ua/image/');
+define('DIR_LANGUAGE', '/var/www/vikki/data/www/prostavki-shop.com.ua/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vikki/data/www/prostavki-shop.com.ua/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/config/');
+define('DIR_CACHE', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vikki/data/www/prostavki-shop.com.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'prostavki_shop');
+define('DB_PASSWORD', 'HfPvqMBxDB1');
+define('DB_DATABASE', 'prostavki_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
